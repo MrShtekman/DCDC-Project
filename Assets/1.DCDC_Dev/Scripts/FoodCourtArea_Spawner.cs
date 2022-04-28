182579846e3c75963e891839a5104f70164bdf62
SharadPoudel <sharadpoudel.info@gmail.com>
2022-04-28 09:56:20 +0200

    Yet to refine

---
+++ Assets/1.DCDC_Dev/Scripts/FoodCourtArea_Spawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCourtArea_Spawner : MonoBehaviour 
+ { 
+ public GameObject FoodcourtPrefab;
+ public GameObject targetModel;
+
+    private const float interval = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       InvokeRepeating("SpawnFoodCourt", 1f,interval);
+    }
+
+    private bool IsModelTracked() {
+        return targetModel.GetComponentInChildren<MeshRenderer>().enabled;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnFoodCourt() {
+        if (IsModelTracked()) {
+            Spawn();
+        }
+    }
+
+    public void Spawn() {
+        GameObject.Instantiate(FoodcourtPrefab, targetModel.transform.position, targetModel.transform.rotation);
+    }
+}
