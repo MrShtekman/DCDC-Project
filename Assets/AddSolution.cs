e11b6e6d5cbce468f99088d1a663b1e7cd9f6e60
SharadPoudel <sharadpoudel.info@gmail.com>
2022-05-11 12:43:19 +0200

    Transition with two image target done
    Needs to fix the resizing.

--- Assets/AddSolution.cs
+++ Assets/AddSolution.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AddSolution : MonoBehaviour
-{
+    {
     public GameObject cube;
-    public GameObject solution;
+    public GameObject foodCourt;
+    [SerializeField] private bool swapped;
+    [SerializeField] private float swapInterval;
+    private float timer;
+    private void Start()
+        {
+        toggleFoodCourt();
+        }
+
+    private void Update()
+        {
+        timer -= Time.deltaTime;
+        }
+
 
     private void OnTriggerEnter(Collider other)
         {
-        cube.SetActive(true);
-        solution.SetActive(false);
-        gameObject.SetActive(false);
+        if(timer <= 0)
+            {
+            swapped = !swapped;
+            toggleFoodCourt();
+            timer = swapInterval;
+            }
+        
         }
+    private void toggleFoodCourt()
+        {
+        cube.SetActive(swapped);
+        foodCourt.SetActive(!swapped);
+        }
+
     }
+
+
