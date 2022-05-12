81fd39e6327c34d3cd313e03b20c40a89bd844e7
SharadPoudel <sharadpoudel.info@gmail.com>
2022-05-12 16:16:56 +0200

    testing version retrieval
    test 1

--- Assets/Scripts/AddSolution.cs
+++ Assets/Scripts/AddSolution.cs
@@ -3,39 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AddSolution : MonoBehaviour
-    {
+{
     public GameObject cube;
-    public GameObject foodCourt;
-    [SerializeField] private bool swapped;
-    [SerializeField] private float swapInterval;
-    private float timer;
-    private void Start()
-        {
-        toggleFoodCourt();
-        }
-
-    private void Update()
-        {
-        timer -= Time.deltaTime;
-        }
-
+    public GameObject solution;
 
     private void OnTriggerEnter(Collider other)
         {
-        if(timer <= 0)
-            {
-            swapped = !swapped;
-            toggleFoodCourt();
-            timer = swapInterval;
-            }
-        
+        cube.SetActive(true);
+        solution.SetActive(false);
+        gameObject.SetActive(false);
         }
-    private void toggleFoodCourt()
-        {
-        cube.SetActive(swapped);
-        foodCourt.SetActive(!swapped);
-        }
-
     }
-
-
