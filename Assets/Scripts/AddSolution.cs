5c57bb1c4f6fa5059a94f5374ca0cbe693a1c974
SharadPoudel <sharadpoudel.info@gmail.com>
2022-05-13 15:01:13 +0200

    Updates from today.

--- Assets/Scripts/AddSolution.cs
+++ Assets/Scripts/AddSolution.cs
@@ -5,8 +5,8 @@ using Unity.AI.Navigation;
 
 public class AddSolution : MonoBehaviour
     {
-    public GameObject cube;
-    public GameObject foodCourt;
+    public GameObject fence;
+    //public GameObject foodCourt;
     [SerializeField] private bool swapped;
     [SerializeField] private float swapInterval;
     [SerializeField] private NavMeshSurface surface;
@@ -28,15 +28,14 @@ public class AddSolution : MonoBehaviour
             {
             swapped = !swapped;
             toggleFoodCourt();
-            surface.BuildNavMesh();
             timer = swapInterval;
             }
         
         }
     private void toggleFoodCourt()
         {
-        cube.SetActive(swapped);
-        foodCourt.SetActive(!swapped);
+        fence.SetActive(swapped);
+        surface.BuildNavMesh();
         }
 
     }
